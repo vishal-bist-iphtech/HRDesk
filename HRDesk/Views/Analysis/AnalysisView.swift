41bca61ca11620336f606572cffdcdf829ec3b44
Vishal Bist <vishal.bist@iphtechnologies.com>
2026-08-13 18:07:30 +0530

    analysis tab added

---
+++ HRDesk/Views/Analysis/AnalysisView.swift
@@ -0,0 +1,16 @@
+//
+//  AnalysisView.swift
+//  HRDesk
+//
+//  Created by iPHTech 34 on 13/08/26.
+//
+
+import SwiftUI
+
+struct AnalysisView: View {
+    
+    var body: some View {
+        Text("Analysis Report")
+            .font(.largeTitle.bold())
+    }
+}
